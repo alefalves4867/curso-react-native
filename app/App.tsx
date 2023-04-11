4da495c10e24079743b9e65963d9519db73e055a
Alef Alves de Araujo <123469430+alefalves4867@users.noreply.github.com>
2023-04-10 22:39:57 -0300

    Initial commit

---
+++ app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import type {PropsWithChildren} from 'react';
+import { Text, View} from 'react-native';
+import C1 from './componentes/comp1';
+
+export default function App(){
+  return(
+    <View>
+      <Text>ברוך הבא!</Text>
+      <C1/>
+    </View>
+  );
+};
---
+++ app/componentes/comp1.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+//import type {PropsWithChildren} from 'react';
+import {Text, View} from 'react-native';
+
+export default function comp1(){
+    return(
+        <Text>Tzion Brazil</Text>
+    )
+}
